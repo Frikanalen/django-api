08ec93a10da1d2b32d5e51cdf24cde0dada8472e
Tore Sinding Bekkedal <toresbe@protonmail.com>
2025-07-09 15:13:13 +0200

    lint: Remove unused import, underscore-prefix unused variables

--- fkweb/signals.py
+++ fkweb/signals.py
@@ -1,5 +1,4 @@
 from django.conf import settings
-from django.contrib.auth import get_user_model
 from django.db.models.signals import post_save
 from django.dispatch import receiver
 from rest_framework.authtoken.models import Token
@@ -8,6 +7,6 @@ from rest_framework.authtoken.models import Token
 # Create a new token for every new user,
 # this is needed for auth to the API
 @receiver(post_save, sender=settings.AUTH_USER_MODEL)
-def create_auth_token(sender, instance=None, created=False, **kwargs):
+def create_auth_token(_sender: str, instance=None, created=False, **_kwargs):
     if created:
         Token.objects.create(user=instance)
