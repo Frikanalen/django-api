fb2c02008f6482a61685664ca1c6091945a7ed0c
Tore Sinding Bekkedal <toresbe@protonmail.com>
2025-07-09 15:39:30 +0200

    remove misleading type annotation

--- fkweb/signals.py
+++ fkweb/signals.py
@@ -1,7 +1,7 @@
 from rest_framework.authtoken.models import Token
 
 
-def create_auth_token(_sender: str, instance=None, created=False, **_kwargs):
+def create_auth_token(_sender, instance=None, created=False, **_kwargs):
     """Create a new token for every new user, this is needed for auth to the API"""
     if created:
         Token.objects.create(user=instance)
