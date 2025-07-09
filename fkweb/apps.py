4af9754504e2ec1012dce28325420c38710c4735
Tore Sinding Bekkedal <toresbe@protonmail.com>
2025-07-09 15:19:34 +0200

    chore: explicitly connect receiver functions

--- fkweb/apps.py
+++ fkweb/apps.py
@@ -1,11 +1,19 @@
 from django.apps import AppConfig
+from django.db.models.signals import post_save
+
+from django.conf import settings
+
+from fkweb.signals import create_auth_token
 
 
 class FrikanalenAppConfig(AppConfig):
     name = "fkweb"
 
     def ready(self):
-        # Register the signal receivers
-        from . import signals
+        # register signal receivers
+        import fkweb.signals  # noqa: F401
 
-        signals  # Fooling flake8
+        post_save.connect(
+            create_auth_token,
+            settings.AUTH_USER_MODEL,
+        )
--- fkweb/signals.py
+++ fkweb/signals.py
@@ -1,12 +1,7 @@
-from django.conf import settings
-from django.db.models.signals import post_save
-from django.dispatch import receiver
 from rest_framework.authtoken.models import Token
 
 
-# Create a new token for every new user,
-# this is needed for auth to the API
-@receiver(post_save, sender=settings.AUTH_USER_MODEL)
 def create_auth_token(_sender: str, instance=None, created=False, **_kwargs):
+    """Create a new token for every new user, this is needed for auth to the API"""
     if created:
         Token.objects.create(user=instance)
