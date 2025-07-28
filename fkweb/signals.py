96fd709749cb000f243d652c301756d346297fad
Tore Sinding Bekkedal <toresbe@protonmail.com>
2025-07-28 19:17:55 +0200

    chore: clean syntax

--- fkweb/signals.py
+++ fkweb/signals.py
@@ -1,8 +1,6 @@
-
-
-
-def create_auth_token(_sender, instance=None, created=False, **_kwargs):
+def create_auth_token(sender=None, instance=None, created=False, **_kwargs):  # noqa: ARG001
     from rest_framework.authtoken.models import Token
+
     """Create a new token for every new user, this is needed for auth to the API"""
     if created:
         Token.objects.create(user=instance)
