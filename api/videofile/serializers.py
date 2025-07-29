bd5140c7a9e056f8714a11c4e2e92b20873e7c72
Tore Sinding Bekkedal <toresbe@protonmail.com>
2025-07-29 03:34:37 +0200

    Add r/w video relation

--- api/videofile/serializers.py
+++ api/videofile/serializers.py
@@ -1,23 +1,21 @@
 from rest_framework import serializers
 
-from fk.models import VideoFile, FileFormat
+from fk.models import VideoFile, FileFormat, Video
 
 
 class VideoFileSerializer(serializers.ModelSerializer):
-    format = serializers.SlugRelatedField(
-        slug_field="fsname",
-        queryset=FileFormat.objects.all(),
-    )
+    format = serializers.SlugRelatedField(slug_field="fsname", queryset=FileFormat.objects.all())
+    video = serializers.PrimaryKeyRelatedField(queryset=Video.objects.all())
 
     class Meta:
         model = VideoFile
         read_only_fields = (
             "id",
-            "video",
             "created_time",
         )
         fields = (
             *read_only_fields,
+            "video",
             "format",
             "filename",
             "integrated_lufs",
