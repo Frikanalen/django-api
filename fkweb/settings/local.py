d17f425f76e3870a2b4cee269c3d8274e0d0aeab
Tore Sinding Bekkedal <toresbe@protonmail.com>
2025-07-29 20:04:08 +0200

    cors: Allow use of the Cache-Control header in dev mode

--- fkweb/settings/local.py
+++ fkweb/settings/local.py
@@ -4,7 +4,7 @@ from .env import env, load_env_from
 
 load_env_from(".env.local")
 from .base import *  # noqa # pylint: disable=unused-import disable=wildcard-import
-
+from corsheaders.defaults import default_headers
 
 FK_UPLOAD_URL = "http://127.0.0.1:5000/upload"
 
@@ -12,5 +12,6 @@ FK_UPLOAD_URL = "http://127.0.0.1:5000/upload"
 # See: https://docs.djangoproject.com/en/dev/ref/settings/#debug
 DEBUG = True
 CORS_ORIGIN_ALLOW_ALL = False
+CORS_ALLOW_HEADERS = (*default_headers, "Cache-Control")
 CORS_ALLOW_CREDENTIALS = True
 CORS_ALLOWED_ORIGINS = env.str("CORS_ALLOWED_ORIGINS", default="http://localhost:3000").split(",")
