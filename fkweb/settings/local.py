20fbf758d9975b527d167e9b754464c265db6a28
Tore Sinding Bekkedal <toresbe@protonmail.com>
2025-08-06 19:57:45 +0200

    dev settings: csrf: kick the tires a little bit

--- fkweb/settings/local.py
+++ fkweb/settings/local.py
@@ -12,6 +12,8 @@ FK_UPLOAD_URL = "http://127.0.0.1:5000/upload"
 # See: https://docs.djangoproject.com/en/dev/ref/settings/#debug
 DEBUG = True
 CORS_ORIGIN_ALLOW_ALL = False
-CORS_ALLOW_HEADERS = (*default_headers, "Cache-Control")
+CORS_ALLOW_HEADERS = (*default_headers, "Cache-Control", "Cookie")
 CORS_ALLOW_CREDENTIALS = True
 CORS_ALLOWED_ORIGINS = env.str("CORS_ALLOWED_ORIGINS", default="http://localhost:3000").split(",")
+CSRF_TRUSTED_ORIGINS = CORS_ALLOWED_ORIGINS
+CSRF_COOKIE_DOMAIN = "http://localhost:3000/"
