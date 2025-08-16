6b767c2d24f4311c15b1606d8d834da3692970f0
Tore Sinding Bekkedal <toresbe@protonmail.com>
2025-08-16 12:51:37 +0200

    Tighten CSRF/Session cookie security

--- fkweb/settings/production.py
+++ fkweb/settings/production.py
@@ -1,3 +1,10 @@
 """Production settings and globals."""
 
 from .base import *
+
+CSRF_COOKIE_SECURE = True
+SESSION_COOKIE_SECURE = True
+SESSION_COOKIE_HTTPONLY = True
+
+# We take traefik's word for whether the request came in as HTTPS
+SECURE_PROXY_SSL_HEADER = ("HTTP_X_FORWARDED_PROTO", "https")
